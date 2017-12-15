207cc6a7018d453e70158b652d2ddfcdc562297e
rishiSan1978 <sandhya.nambiar@dzeesolutions.com>
2017-12-14 23:05:26 -0700

    first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp(){
+		
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+        driver =  new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin(){
+		 driver.get("https://www.google.com/");
+	}
+	
+	@AfterTest
+	public void tearDown(){
+	   driver.quit();		
+	}
+}
